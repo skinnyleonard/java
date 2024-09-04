a4da61110e4626b93d7c8c1769d502bb162ea8c9
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-09-04 12:49:05 -0300

    Update Clase1.java

--- Clase1.java
+++ Clase1.java
@@ -1,16 +1,19 @@
-package mi_primer_programa_de_java;
+package javascript;
 
 import java.util.Scanner;
 
 public class Clase1 {
 	public static void main(String[] args) {
+		//ejercicio 1
 		int count = 0;
-		/*for(int i=0; i<102; i++) {
+		for(int i=0; i<102; i++) {
 			if(i%2 ==0) {
 				System.out.println("el numero " + i + " es par");
 			}
-		}*/
-		/*for(int i=50; i<=100; i++) {
+		}
+		
+		//ejercicio 2
+		for(int i=50; i<=100; i++) {
 			count=0;
 			for(int j=2; j<=i/2; j++) {
 				if(i%j ==0) {
@@ -21,11 +24,45 @@ public class Clase1 {
 		if(count==0) {
 			System.out.println(i);
 		}
-		}*/
-		Scanner num = new Scanner(System.in);
-		System.out.println("escribi un numero");
-		String number = num.nextLine();
-		System.out.println("tu numero es: " + number);
+		}
 		
+		//ejercicio 4
+		double num1, num2;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa el numero");
+        num1 = sc.nextDouble();
+        System.out.println("ingresa el operador");
+        char op = sc.next().charAt(0);
+        System.out.println("ingresa el numero");
+        num2 = sc.nextDouble();
+        
+        double calc = 0;
+        
+        switch (op) {
+            case '+':
+                calc = num1 + num2;
+                break;
+            case '-':
+                calc = num1 - num2;
+                break;
+            case '*':
+                calc = num1 * num2;
+                break;
+            case '/':
+                calc = num1 / num2;
+                break;
+            default:
+                System.out.println("no es un caracter valido");
+                break;
+        }
+        System.out.println("resultado final: " + calc);
+		
+      //ejercicio 5
+		/*for-each*/
+		String[] autos = {"Ford", "Chevrolet", "Fiat", "Bugatti"};
+		/*for (String auto: autos){
+			System.out.println(auto);
+		}*/
 	}
 }
