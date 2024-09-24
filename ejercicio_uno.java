942da9d657089c7bdeff057f4dd9a6cc33d66d3d
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-09-24 14:33:49 -0300

    Add files via upload

---
+++ ejercicio_uno.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class ejercicio_uno {
+	public static void main(String[] args) {
+		int n1 = 32;
+		int n2 = 16;
+		
+		System.out.println("La suma es: " + (n1 + n2));
+		System.out.println("La resta es: " + (n1 - n2));
+		System.out.println("La multiplicacion es: " + (n1 * n2));
+		System.out.println("La division es: " + (n1 / n2));
+	}
+}
