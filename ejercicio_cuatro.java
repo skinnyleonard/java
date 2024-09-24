4d61a5ce47eb8fe5764e00bdd7f282d204107358
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-09-24 15:03:16 -0300

    Add files via upload

---
+++ ejercicio_cuatro.java
@@ -0,0 +1,10 @@
+package ej4;
+
+import javax.swing.JOptionPane;
+public class ejercicio_cuatro {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Introduci tu nombre");
+		
+		System.out.println("Bienvenido " + name);
+	}
+}
