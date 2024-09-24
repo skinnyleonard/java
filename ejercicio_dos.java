3fc024204f8fc0f7e91a0c59d03a47462d496e84
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-09-24 14:47:20 -0300

    Add files via upload

---
+++ ejercicio_dos.java
@@ -0,0 +1,18 @@
+package ejercicio_dos;
+
+public class ejercicio_dos {
+	public static void main(String[] args) {
+		int n1 = 234;
+		int n2 = 123;
+		
+		if (n1 > n2 ) {
+			System.out.println("El numero " + n1 + " es mayor");
+		}
+		else if(n2 > n1) {
+			System.out.println("El numero " + n2 + " es mayor");
+		}
+		else if (n1 == n2) {
+			System.out.println("Son iguales");
+		}
+	}
+}
